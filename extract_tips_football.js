c62848fd54b77ac7a128002863d56edf639966f6
Kendry Jose Ortiz Pastrana <kendry.ortiz@sophossolutions.com>
2023-10-04 23:08:04 -0500

    Agregando mejoras para exportar un excel con los pronosticos

--- extract_tips_football.js
+++ extract_tips_football.js
@@ -2,12 +2,32 @@ const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 const fs = require("fs");
 const moment = require('moment');
-const { exit } = require("process");
-
+const xl = require('excel4node');
+const path = require('path');
 
 const DATE = process.env.DATE || moment().format("YYYY-MM-DD")
 
 console.log("DATE >>>>", DATE);
+
+var wb = new xl.Workbook();
+
+// Crear estilos
+const cualColumnaEstilo = wb.createStyle({
+    font: {
+        name: 'Arial',
+        color: '#000000',
+        size: 12,
+        bold: true,
+    }
+});
+
+const contenidoEstilo = wb.createStyle({
+    font: {
+        name: 'Arial',
+        color: '#494949',
+        size: 11,
+    }
+});
 
 // function to get the raw data
 const getRawData = (URL) => {
@@ -72,6 +92,20 @@ const scrapeData = async () => {
 
     const percentages = [];
     if (competitions.length > 0) {
+
+        let nombreArchivo = "juegos";
+        let ws = wb.addWorksheet(nombreArchivo);
+        let lineData = 2;
+
+        //Nombres de las columnas
+        ws.cell(1, 1).string("#").style(cualColumnaEstilo);
+        ws.cell(1, 2).string("Fecha").style(cualColumnaEstilo);
+        ws.cell(1, 3).string("Local").style(cualColumnaEstilo);
+        ws.cell(1, 4).string("Visitante").style(cualColumnaEstilo);
+        ws.cell(1, 5).string("% acierto").style(cualColumnaEstilo);
+        ws.cell(1, 6).string("Pronostico").style(cualColumnaEstilo);
+        ws.cell(1, 7).string("Resultado").style(cualColumnaEstilo);
+
         console.log("# --- Date --- Team1 --- Team2 --- Tip --- % Tip --- Result");
         let n = 0;
         for (let i = 0; i < competitions.length; i++) {
@@ -121,6 +155,34 @@ const scrapeData = async () => {
                     }
                     const row = `${n} --- ${date} --- ${team1} (${betTeam1}) (${goalsTeam1}) --- ${team2} (${betTeam2}) (${goalsTeam2}) --- ${tip} --- ${percentTip}% --- ${resultTip}`;
                     console.log(row);
+
+                    // Construcción del excel
+                    // Nombre
+                    ws.cell(lineData, 1).number(n).style(contenidoEstilo);
+                    // apellido
+                    ws.cell(lineData, 2).string((DATE + ' ' + date).toString()).style(contenidoEstilo);
+                    // edad
+                    ws.cell(lineData, 3).string((team1)).style(contenidoEstilo);
+                    // id
+                    ws.cell(lineData, 4).string(team2).style(contenidoEstilo);
+                    // teléfono
+                    ws.cell(lineData, 5).string(percentTip + '%').style(contenidoEstilo);
+
+                    let tipExcel = tip == '1' ? team1 : team2;
+                    // correo
+                    ws.cell(lineData, 6).string(tipExcel).style(contenidoEstilo);
+
+                    let resultTipExcel = 'N/A';
+                    if (resultTip == 'YES') {
+                        resultTipExcel = 'ACERTADO';
+                    } else if (resultTip == 'NO') {
+                        resultTipExcel = 'FALLADO';
+                    }
+
+                    ws.cell(lineData, 7).string(resultTipExcel).style(contenidoEstilo);
+
+                    // Aumenta de fila
+                    lineData++;
                     if (countTypeResult.yes < 4 && resultTip == "YES") {
                         top3.push(row)
                     }
@@ -140,6 +202,25 @@ const scrapeData = async () => {
         } else {
             console.log("----------- NO EXISTEN JUEGOS CON ALTAS PROBABILIDADES -----------");
         }
+
+        //Ruta del archivo
+        const pathExcel = path.join(__dirname, 'excel', nombreArchivo + '.xlsx');
+        //Escribir o guardar
+        wb.write(pathExcel, function(err, stats){
+            if(err) console.log(err);
+            else{
+
+                // Crear función y descargar archivo
+                function downloadFile(){res.download(pathExcel);}
+                //downloadFile();
+
+                // Borrar archivo
+                /* fs.rm(pathExcel, function(err){
+                    if(err) console.log(err);
+                    else  console.log("Archivo descargado y borrado del servidor correctamente");
+                }); */
+            }
+        });
 
 
     } else {
